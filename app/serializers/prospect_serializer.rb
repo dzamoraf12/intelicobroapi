35704182cef79bf1d54fc8ee63d26ff12613d3fc
Daniel Zamora <daniel.zamoraf@gmail.com>
2023-06-26 22:51:24 -0700

    Add created_at to prospect serializer

--- app/serializers/prospect_serializer.rb
+++ app/serializers/prospect_serializer.rb
@@ -3,5 +3,6 @@ class ProspectSerializer < Blueprinter::Base
   fields :names, :mother_surname, :father_surname, :street, :internal_number,
          :external_number, :zip_code, :neighborhood, :municipality, :city,
          :state, :latitude, :longitude, :verification_status, :verified_at,
-         :verification_accepted_at, :verification_rejected_at, :agent_id
+         :verification_accepted_at, :verification_rejected_at, :agent_id,
+         :created_at
 end
